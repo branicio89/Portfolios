0ff9360c9aa43fbfc46b009e0c4388b884c54ecf
Paulo <pb.paulo89@gmail.com>
2024-01-17 10:15:12 -0300

    commit full course

---
+++ menu.js
@@ -0,0 +1,15 @@
+let btmenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btmenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
